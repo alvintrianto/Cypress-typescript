7a53bba00754552dc3da90605956e488a4aae3ce
ziassy <fauziahfz216@gmail.com>
2023-07-11 21:23:18 +0700

    [UPDATE] delete .peek for Assertion login

--- cypress/integration/login.ts
+++ cypress/integration/login.ts
@@ -1,22 +1,22 @@
-import {LoginPage} from "./pages/login_pages"
-import {DashboardPage} from "./pages/dashboard_pages"
+import { LoginPage } from "./pages/login_pages"
+import { DashboardPage } from "./pages/dashboard_pages"
 
 let loginPage = new LoginPage()
 let dashboardPage = new DashboardPage()
-const URL = 'https://www.saucedemo.com/'
+const URL = 'https://www.saucedemo.com'
 
 it('Test LOGIN', () => {
-    loginPage.login(URL,'standard_user','secret_sauce')
-    loginPage.assertLogin() 
+    loginPage.login(URL, 'standard_user', 'secret_sauce')
+    loginPage.assertLogin()
 })
 
 it('Test Sauce Demo Invalid Password', () => {
-    loginPage.login(URL,'standard_user','invalidPass')
+    loginPage.login(URL, 'standard_user', 'invalidPass')
     loginPage.assertLoginFail()
 })
 
 it('Test Sauce Demo Sauce labs product backpack', () => {
-    loginPage.login(URL,'standard_user','secret_sauce')
+    loginPage.login(URL, 'standard_user', 'secret_sauce')
     loginPage.assertLogin()
-    dashboardPage.sauceLabsBackpack() 
+    dashboardPage.sauceLabsBackpack()
 })
--- cypress/integration/pages/login_pages.ts
+++ cypress/integration/pages/login_pages.ts
@@ -1,31 +1,30 @@
 import { JsxEmit } from "typescript"
 
-export class LoginPage{
+export class LoginPage {
     txt_username = '#user-name'
     txt_password = '#password'
     btn_login = '#login-button'
 
-    navigate(url: string){
+    navigate(url: string) {
         cy.visit(url)
     }
-    inputUsername(username: string){
+    inputUsername(username: string) {
         cy.get(this.txt_username).type(username)
     }
-    inputPassword(password: string){
+    inputPassword(password: string) {
         cy.get(this.txt_password).type(password)
     }
-    clickLogin(){
+    clickLogin() {
         cy.get(this.btn_login).click()
     }
-    assertLogin(){
+    assertLogin() {
         cy.get('.title').should('be.visible')
-        cy.get('.peek').should('be.visible')
-        cy.contains('Products').should('contain','Products')
+        cy.contains('Products').should('contain', 'Products')
     }
-    assertLoginFail(){
+    assertLoginFail() {
         cy.get('[data-test="error"]').should('be.visible')
     }
-    login(url : string,username : string,password : string){
+    login(url: string, username: string, password: string) {
         this.navigate(url)
         this.inputUsername(username)
         this.inputPassword(password)
